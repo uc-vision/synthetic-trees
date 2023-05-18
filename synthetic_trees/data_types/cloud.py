a12c1a6692c9c91b687ab643a32a817c2423dcdc
harry1576 <harrydobbs87@gmail.com>
2023-05-19 08:34:07 +1200

    Fixed colour mapping for multi class clouds

--- synthetic_trees/data_types/cloud.py
+++ synthetic_trees/data_types/cloud.py
@@ -14,10 +14,16 @@ class Cloud:
         return xyz.shape[0]
 
     def __str__(self):
-        return f'Cloud with {self.xyz.shape[0]} points '
+        return f"Cloud with {self.xyz.shape[0]} points "
 
     def to_o3d_cloud(self):
         return o3d_cloud(self.xyz, colours=self.rgb)
 
-    def to_o3d_cloud_labelled(self, cmap=np.asarray([[0.53, 0.24, 0.13], [0.64, 0.92, 0.20], [1.0, 0.5, 0.3], [0.8, 1.0, 0.6]])):
+    def to_o3d_cloud_labelled(self, cmap=None):
+        if cmap == None:
+            cmap = np.random.rand(self.number_classes, 3)
         return o3d_cloud(self.xyz, colours=cmap[self.class_l])
+
+    @property
+    def number_classes(self):
+        return np.max(self.class_l) + 1
